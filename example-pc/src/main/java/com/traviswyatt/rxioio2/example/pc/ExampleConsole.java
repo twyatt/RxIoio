25d263820aa04526354b00b1c2da10454314b562
Travis Wyatt <travis.i.wyatt@gmail.com>
2017-04-06 00:03:19 -0700

    Use blocking Observable in console example

--- example-pc/src/main/java/com/traviswyatt/rxioio2/example/pc/ExampleConsole.java
+++ example-pc/src/main/java/com/traviswyatt/rxioio2/example/pc/ExampleConsole.java
@@ -2,11 +2,11 @@ package com.traviswyatt.rxioio2.example.pc;
 
 import com.traviswyatt.rxioio2.RxIoio;
 import io.reactivex.Observable;
+import io.reactivex.Observer;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.BiFunction;
 import io.reactivex.functions.Function;
-import io.reactivex.observers.DisposableObserver;
 import ioio.lib.api.IOIO;
 import ioio.lib.api.exception.ConnectionLostException;
 import ioio.lib.util.IOIOLooper;
@@ -33,6 +33,7 @@ public class ExampleConsole implements IOIOLooperProvider {
         }
     }
 
+    /** Stop IOIO and shutdown application. */
     private synchronized void done() {
         done = true;
         notifyAll();
@@ -42,14 +43,14 @@ public class ExampleConsole implements IOIOLooperProvider {
     public IOIOLooper createIOIOLooper(String connectionType, Object extra) {
         return new IOIOLooper() {
             private static final int STAT_LED_PIN = 0;
-            private Disposable disposable;
 
             @Override
             public void setup(IOIO ioio) throws ConnectionLostException, InterruptedException {
+                System.out.println("connected");
                 RxIoio rxIoio = RxIoio.create(ioio);
 
                 // Basic example that blinks IOIO stat LED 5 times.
-                disposable = Observable
+                Observable
                         .range(1, 10)
                         .map(new Function<Integer, Boolean>() {
                             @Override
@@ -64,7 +65,11 @@ public class ExampleConsole implements IOIOLooperProvider {
                             }
                         })
                         .lift(rxIoio.digitalOutput(STAT_LED_PIN))
-                        .subscribeWith(new DisposableObserver<Boolean>() {
+                        .blockingSubscribe(new Observer<Boolean>() {
+                            @Override public void onSubscribe(Disposable d) {
+                                // no-op
+                            }
+
                             @Override
                             public void onNext(Boolean aBoolean) {
                                 System.out.println("Setting stat LED pin to: " + aBoolean);
@@ -78,31 +83,25 @@ public class ExampleConsole implements IOIOLooperProvider {
                             @Override
                             public void onComplete() {
                                 System.out.println("Done");
-                                done();
                             }
                         });
 
-                System.out.println("setup done");
+                done();
             }
 
             @Override
             public void loop() throws ConnectionLostException, InterruptedException {
-                System.out.println("loop!");
                 Thread.sleep(1000L);
             }
 
             @Override
             public void disconnected() {
-                if (disposable != null) {
-                    disposable.dispose();
-                }
+                System.out.println("disconnected");
             }
 
             @Override
             public void incompatible() {
-                if (disposable != null) {
-                    disposable.dispose();
-                }
+                System.out.println("incompatible");
             }
 
             @Override
